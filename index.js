e63bb2bb4ca930c9bd385d7c2e7d567d54c846aa
Harry Wolff <hswolff@gmail.com>
2016-01-19 09:40:55 -0500

    Allow individual files to configure this plugin, whether it's on or off.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var minify = require('html-minifier').minify;
 
 module.exports = function(Plugin, options) {
-  Plugin.event.collection.beforeWrite(function(fileContents) {
-    fileContents.content = minify(fileContents.content, Plugin.merge({
+  Plugin.event.collection.beforeWrite(function(file, fileContent) {
+    // Check if individual File disables or changes options of this plugin.
+    var filePluginConfig = file.data && file.data.plugins &&
+        file.data.plugins['html-minifier'] ?
+        file.data.plugins['html-minifier'] : {};
+
+    if (filePluginConfig.enabled === false) {
+      return;
+    }
+
+    return [file, minify(fileContent, Plugin.merge({
       collapseBooleanAttributes: true,
       collapseWhitespace: true,
       removeAttributeQuotes: true,
       removeComments: true,
       removeEmptyAttributes: true,
       removeRedundantAttributes: true
-    }, options));
+    }, options, filePluginConfig.options))];
   });
 };
