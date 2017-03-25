87fda90d880e6aca874ae0672fe8275cc97dfe39
Harry Wolff <hswolff@gmail.com>
2017-03-25 17:37:35 -0400

    fix: Update to be Reptar v3.x compatible

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-var minify = require('html-minifier').minify;
+const minify = require('html-minifier').minify;
+const path = require('path');
+const fs = require('fs');
 
-module.exports = function(Plugin, options) {
-  function handler(file, fileContent) {
-    // Check if individual File disables or changes options of this plugin.
-    var filePluginConfig = file.data && file.data.plugins &&
-        file.data.plugins['html-minifier'] ?
-        file.data.plugins['html-minifier'] : {};
-
-    if (filePluginConfig.enabled === false) {
-      return;
+module.exports = function htmlMinifierMiddleware(reptar) {
+  for (const relativePath in reptar.destination) {
+    if (path.extname(relativePath) !== '.html') {
+      continue;
     }
 
-    return [file, minify(fileContent, Plugin.merge({
+    const file = reptar.destination[relativePath];
+
+    if (file.filtered) {
+      continue;
+    }
+
+    const destinationPath = path.join(
+      reptar.config.get('path.destination'),
+      relativePath
+    );
+
+    var fileContent;
+    try {
+      fileContent = fs.readFileSync(destinationPath, 'utf8');
+    } catch (e) {
+      console.log(file);
+      throw e;
+    }
+
+    const newContent = minify(fileContent, {
       collapseBooleanAttributes: true,
       collapseWhitespace: true,
       removeAttributeQuotes: true,
       removeComments: true,
       removeEmptyAttributes: true,
-      removeRedundantAttributes: true
-    }, options, filePluginConfig.options))];
-  };
+      removeRedundantAttributes: true,
+    });
 
-  Plugin.event.file.afterRender(handler);
-  Plugin.event.page.afterRender(handler);
-};
+    fs.writeFileSync(destinationPath, newContent, 'utf8');
+  }
+}
